0e957aa35209c515553fc93d205b9e44dc47e040
leevic31 <leevic31@gmail.com>
2024-07-16 18:10:03 -0400

    added note model test

--- test/models/note_test.rb
+++ test/models/note_test.rb
@@ -1,7 +1,17 @@
 require "test_helper"
 
 class NoteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = users(:user_one)
+    @note = Note.new(content: "note content", user_id: @user.id)
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+  test "content should be present" do
+    @note.content = ""
+    assert_not @note.valid?
+  end
 end
